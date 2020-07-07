be9a8b87b579fb82111c20dc662d4d0dce508d63
Matthias Geisler <matthias@openwebcraft.com>
2020-07-07 10:41:23 +0200

    fixed undefined var

--- slack.go
+++ slack.go
@@ -64,11 +64,10 @@ func (a *SlackAdapter) Stream(logstream chan *router.Message) {
 		User:     a.slackUser,
 		Username: a.slackUsername,
 	}
-	msgOptionPostMsgParams := slack.MsgOptionPostMessageParameters(msgParams)
 	fmt.Printf("%+v", a)
 	for message := range logstream {
 		if ok, _ := regexp.MatchString(a.messageFilter, message.Data); ok {
-			a.slackClient.PostMessage(a.slackChannel, slack.MsgOptionText(message.Data, false), msgOptionPostMsgParams)
+			a.slackClient.PostMessage(a.slackChannel, slack.MsgOptionText(message.Data, false), slack.MsgOptionPostMessageParameters(msgParams))
 		}
 	}
 }
