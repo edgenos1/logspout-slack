32b786f1627d5f9ebc92b55793ad7ee998d0ba2b
Luc Claustres <luc.claustres@orange.fr>
2020-10-20 19:29:21 +0200

    Updated code to use webhook

--- slack.go
+++ slack.go
@@ -24,25 +24,14 @@ func getopt(name, dfault string) string {
 
 // NewSlackAdapter creates a Slack adapter.
 func NewSlackAdapter(route *router.Route) (router.LogAdapter, error) {
-	slackToken := getopt("SLACK_TOKEN", route.Options["slack_token"])
-	slackUser := getopt("SLACK_USER", route.Options["slack_user"])
-	slackUsername := getopt("SLACK_USERNAME", route.Options["slack_username"])
-	slackChannel := getopt("SLACK_CHANNEL", route.Options["slack_channel"])
+	slackWebhook := getopt("SLACK_WEBHOOK_URL", route.Options["slack_webhook_url"])
 	messageFilter := getopt("SLACK_MESSAGE_FILTER", route.Options["slack_message_filter"])
 	if messageFilter == "" {
 		messageFilter = "*"
 	}
 
-	slackClient := slack.New(slackToken)
-	if slackClient == nil {
-		return nil, errors.New("unable to get slack client: " + route.Adapter)
-	}
-
 	return &SlackAdapter{
-		slackClient:   slackClient,
-		slackChannel:  slackChannel,
-		slackUser:     slackUser,
-		slackUsername: slackUsername,
+		slackWebhook:   slackWebhook,
 		messageFilter: messageFilter,
 		route:         route,
 	}, nil
@@ -50,24 +39,18 @@ func NewSlackAdapter(route *router.Route) (router.LogAdapter, error) {
 
 // SlackAdapter describes a Slack adapter
 type SlackAdapter struct {
-	slackClient   *slack.Client
-	slackUser     string
-	slackUsername string
-	slackChannel  string
+	slackWebhook  string
 	messageFilter string
 	route         *router.Route
 }
 
 // Stream implements the router.LogAdapter interface.
 func (a *SlackAdapter) Stream(logstream chan *router.Message) {
-	msgParams := slack.PostMessageParameters{
-		User:     a.slackUser,
-		Username: a.slackUsername,
-	}
-	fmt.Printf("%+v", a)
 	for message := range logstream {
 		if ok, _ := regexp.MatchString(a.messageFilter, message.Data); ok {
-			a.slackClient.PostMessage(a.slackChannel, slack.MsgOptionText(message.Data, false), slack.MsgOptionPostMessageParameters(msgParams))
+			slack.PostWebhook(a.slackWebhook, slack.WebhookMessage{
+				Text:     message.Data
+			})
 		}
 	}
 }
